88143d15cb1df795d4e908505a0dbca5642324c1
ShinYooJung <shinyoojung0@gmail.com>
2021-04-02 15:17:16 +0900

    initialize project

---
+++ src/main/java/org/cnu/realcoding/MyFirstSpringBootApp2Application.java
@@ -0,0 +1,13 @@
+package org.cnu.realcoding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFirstSpringBootApp2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFirstSpringBootApp2Application.class, args);
+	}
+
+}
