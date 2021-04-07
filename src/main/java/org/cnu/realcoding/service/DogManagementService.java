2332144e81fa1e7c8b82a01dacf7b17bc2509ed6
YuMi <92506_@naver.com>
2021-04-08 01:30:38 +0900

    과제 3번 (unique key를 입력 받아 Dog객체  구별) 강아지 종 수정, 강아지 정보 통째로 수정, 진료기록 추가 기능 구현

--- src/main/java/org/cnu/realcoding/service/DogManagementService.java
+++ src/main/java/org/cnu/realcoding/service/DogManagementService.java
@@ -5,6 +5,8 @@ import org.cnu.realcoding.repository.DogRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class DogManagementService {
 
@@ -13,6 +15,18 @@ public class DogManagementService {
 
     public void insertDog(Dog dog) {
         dogRepository.insertDog(dog);
+    }
+
+    public void updateAllDog(String name, String ownerName, String ownerPhoneNumber, String cName, String cKind, String cOwnerName, String cOwnerPhoneNumber) {
+        dogRepository.updateAllDog(name, ownerName, ownerPhoneNumber, cName, cKind, cOwnerName, cOwnerPhoneNumber);
+    }
+
+    public void updateDog(String name, String ownerName, String ownerPhoneNumber, String cKind) {
+        dogRepository.updateDog(name, ownerName, ownerPhoneNumber, cKind);
+    }
+
+    public void plusRecord(String name, String ownerName, String ownerPhoneNumber, String record) {
+        dogRepository.plusRecord(name, ownerName, ownerPhoneNumber, record);
     }
 
 }
